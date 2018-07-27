4176187aeab051579b23c844de3e8d380be8f512
ErunaM <DeceptiveSmurf@gmail.com>
2018-07-27 21:10:49 +1200

    added Router links so people can work on individual pages

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
 import NavBar from './page1/navBar';
+import Login from './Login/login';
 
 class App extends Component {
   render() {
     return(
-      <NavBar />
+      <div>
+        <BrowserRouter>
+            <div>
+              <Route exact path="/" component={NavBar}/>
+              <Route exact path="/login" component={Login}/>
+
+            </div>
+        </BrowserRouter>
+
+      </div>
 
     );
   }
