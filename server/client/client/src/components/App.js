77e0f9a1de7d25ebf8d14bf32c7f60155f66d10d
ErunaM <DeceptiveSmurf@gmail.com>
2018-07-27 14:25:25 +1200

    test

---
+++ server/client/client/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import NavBar from './page1/navBar';
+
+class App extends Component {
+  render() {
+    return(
+      <NavBar />
+
+    );
+  }
+}
+
+export default App;
