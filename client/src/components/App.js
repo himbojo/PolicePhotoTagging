c328754af73bfac05a5d2680c32862b5c27648e5
Andy <Andy@DESKTOP-IKHIJ59>
2018-07-31 13:10:59 +1200

    Basic search page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import NavBar from './page1/navBar';
 import Login from './Login/login';
+import Search from './Search/search';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
             <div>
               <Route exact path="/" component={NavBar}/>
               <Route exact path="/login" component={Login}/>
+              <Route exact path="/search" component={Search}/>
 
             </div>
         </BrowserRouter>
