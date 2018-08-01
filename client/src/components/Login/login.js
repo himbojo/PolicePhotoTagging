f5f5b7a7aa1471ca7b74ff30eb5df7756c8073de
ErunaM <DeceptiveSmurf@gmail.com>
2018-08-02 01:39:30 +1200

    Updated login screen to have the layout i like, just gotta make fix a few things and then all done

--- client/src/components/Login/login.js
+++ client/src/components/Login/login.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
+import Background from '../../pictures/loginBG.jpg';
 import LoginForm from './loginForm';
 
 class Login extends Component{
   render(){
-return(<div>
+return(<div className="loginMain" style={{ backgroundImage: `url(${Background})`, height: "100vh" }}>
       <LoginForm />
-  
+
     </div>);
   }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
+import './loginCss.css';
 import 'materialize-css/dist/css/materialize.css';
 
 import App from './components/App';
