876849247a26615c0f5a5f88cc8125afb1361098
Andy <Andy@DESKTOP-IKHIJ59>
2018-08-06 13:41:17 +1200

    Made search page

---
+++ client/src/components/Search/searchForm.js
@@ -0,0 +1,128 @@
+import React, { Component } from "react";
+import {
+  FormControl,
+  FormGroup,
+  ControlLabel,
+  Grid,
+  Row,
+  Col,
+  Button
+} from "react-bootstrap";
+import { BrowserRouter, Route } from "react-router-dom";
+import ImageUpload from "../objects/imageUpload";
+import DateOnlyPicker from "../objects/dateOnlyPicker";
+import TimeOnlyPicker from "../objects/timeOnlyPicker";
+import Tagging from "../objects/tagging";
+import "./inputForm.css";
+import "./search.css";
+
+class SearchForm extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      qid: "",
+      filenumber: "",
+      location: "",
+      tags: "",
+      description: "",
+      offence: "",
+      showing: false
+    };
+  }
+
+  isNumber(_value) {
+    var value = _value;
+    if (!isNaN(parseInt(value))) return "success";
+    else if (value === "") return null;
+    else return "error";
+  }
+
+  isNull(_value) {
+    var value = _value;
+    if (value === "") return null;
+    else return "success";
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  render() {
+    const { showing } = this.state;
+    return (
+      <div>
+        <Grid>
+          <Row xsOffset={1} xs={3} className="colStyle1">
+            <form>
+              <FormGroup>
+                <ControlLabel>Search Tags</ControlLabel>
+                <Tagging />
+              </FormGroup>
+
+              <div />
+
+              {showing ? (
+                <div>
+                  <FormGroup validationState={this.isNumber(this.state.qid)}>
+                    <ControlLabel>QID</ControlLabel>
+                    <FormControl
+                      type="text"
+                      name="qid"
+                      placeholder="Please enter QID"
+                      value={this.state.value}
+                      onChange={this.handleChange}
+                    />
+                  </FormGroup>
+                  <FormGroup
+                    validationState={this.isNumber(this.state.filenumber)}
+                  >
+                    <ControlLabel>File Number</ControlLabel>
+                    <FormControl
+                      type="text"
+                      name="filenumber"
+                      placeholder="Please enter FileNumber"
+                      value={this.state.value}
+                      onChange={this.handleChange}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <ControlLabel>Date</ControlLabel>
+                    <DateOnlyPicker />
+                  </FormGroup>
+                  <FormGroup>
+                    <ControlLabel>Time</ControlLabel>
+                    <TimeOnlyPicker />
+                  </FormGroup>
+
+                  <FormGroup validationState={this.isNull(this.state.offence)}>
+                    <ControlLabel>Offence</ControlLabel>
+                    <FormControl
+                      type="text"
+                      name="offence"
+                      placeholder="Please enter Offence"
+                      value={this.state.value}
+                      onChange={this.handleChange}
+                    />
+                  </FormGroup>
+                </div>
+              ) : null}
+              <Button bsStyle="primary">Search</Button>
+              <Button
+                className="refineButton"
+                bsStyle="primary"
+                bsSize="small"
+                onClick={() => this.setState({ showing: !showing })}
+              >
+                Refine Search
+              </Button>
+            </form>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default SearchForm;
