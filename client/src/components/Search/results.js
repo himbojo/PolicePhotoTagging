1d4e054f708d9050b7a737b4f2788cc9964376f3
Jordan Akroyd <jordan.akroyd@gmail.com>
2018-09-30 23:04:22 +1300

    loaded more data into results modal and fixed the displaying of the date, changed some more css, removed unused files

--- client/src/components/Search/results.js
+++ client/src/components/Search/results.js
@@ -3,15 +3,12 @@ import React, { Component } from "react";
 import "./upload.css";
 import "../css/main.css";
 
-import NavBar from "../objects/navBar";
-
 import ResultForm from "./resultForm";
 
 class Results extends Component {
   render() {
     return (
       <div className="containerBox">
-        <NavBar />
         <ResultForm />
       </div>
     );
--- client/src/components/Search/search.js
+++ client/src/components/Search/search.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 
 import "./upload.css";
 import "../css/main.css";
-
-import NavBar from "../objects/navBar";
 import SearchForm from "./searchForm";
 import SearchMap from "./searchMap";
 
@@ -32,7 +30,6 @@ class Search extends Component {
   render() {
     return (
       <div className="containerBox">
-        <NavBar />
         <div className="backgroundImage">
           <div className="mainPage">
             <SearchForm onShowMap={this.onShowMap}/>
