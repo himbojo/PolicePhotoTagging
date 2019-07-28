18872f2c3e28d477c9b4e3332f99fe5716636a65
Jordan Akroyd <36215472+JBAkroyd@users.noreply.github.com>
2019-07-29 09:58:17 +1200

    Update dev.js

--- dev.js
+++ dev.js
@@ -1,9 +1,9 @@
 // ADD TO .GITIGNORE WHEN UPLOADING TO AWS
 module.exports = {
-  DB_HOST_NAME: 'policephototagging.cxtufkjuvqzf.ap-southeast-2.rds.amazonaws.com',
-  DB_ADMIN_USER: 'admin',
-  DB_ADMIN_PASS: 'admin123',
-  DB_NAME: 'PolicePhotoTagging',
-  S3_ACCESS_KEY_ID: 'AKIAIPC5WDUR6SXASWTQ',
-  S3_ACCESS_KEY: 'GTkj/LT4nh7+eItXUZbkFrtn1xHCvg0XM0jIfCrO'
+  DB_HOST_NAME: '',
+  DB_ADMIN_USER: '',
+  DB_ADMIN_PASS: '',
+  DB_NAME: '',
+  S3_ACCESS_KEY_ID: '',
+  S3_ACCESS_KEY: ''
 };
